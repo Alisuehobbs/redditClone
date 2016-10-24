b2ec18cbcb78a4909a267bd3223f7f9d49349b99
Ali Hobbs <alisuehobbs@gmail.com>
2016-10-24 14:08:08 -0600

    added date stamp

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ app.controller("redditController", ($scope) => {
   title: "Arancini",
   author: "Ali Sue Hobbs",
   imgURL: "http://christinascucina.com/wp-content/uploads/2013/03/IMG_1742.jpg",
-  description: "Arancini ([aranˈtʃiːni]; Italian: arancino in singular, Sicilian: arancini or arancine) are stuffed rice balls which are coated with bread crumbs and then deep fried. They are usually filled with ragù (meat and tomato sauce), mozzarella, and peas. A number of regional variants exist which differ in fillings and shape. The name, which is translated as 'little orange', derives from their shape and colour, which is reminiscent of an orange after cooking. Arancini produced in eastern Sicily (especially in Catania) have a more conical shape."
+  description: "Arancini ([aranˈtʃiːni]; Italian: arancino in singular, Sicilian: arancini or arancine) are stuffed rice balls which are coated with bread crumbs and then deep fried. They are usually filled with ragù (meat and tomato sauce), mozzarella, and peas. A number of regional variants exist which differ in fillings and shape. The name, which is translated as 'little orange', derives from their shape and colour, which is reminiscent of an orange after cooking. Arancini produced in eastern Sicily (especially in Catania) have a more conical shape.",
+  date: '2016-10-24T13:24:00'
   }]
 
   $scope.submitPost = function(post) {
     console.log('post:', post);
     event.preventDefault()
     if (post) {
+      $scope.post.date = new Date()
       $scope.posts.push($scope.post)
       console.log('$scope.post:', $scope.post);
       console.log('$scope.posts', $scope.posts);
@@ -20,5 +22,7 @@ app.controller("redditController", ($scope) => {
       $scope.newPost.$setPristine()
     }
   }
+
+  $scope.sort = "title"
 
 })
