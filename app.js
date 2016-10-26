6840bf79b9257de96cd4f2b985631862f48cc574
Ali Hobbs <alisuehobbs@gmail.com>
2016-10-26 11:37:36 -0600

    completed all requirements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const app = angular.module("redditApp", []);
+const app = angular.module("redditApp", ['ngAnimate']);
 app.controller("redditController", ($scope) => {
     $scope.sort = "-votes"
 
@@ -10,7 +10,7 @@ app.controller("redditController", ($scope) => {
         author: "Ali Sue Hobbs",
         imgURL: "http://christinascucina.com/wp-content/uploads/2013/03/IMG_1742.jpg",
         description: "Arancini ([aranˈtʃiːni]; Italian: arancino in singular, Sicilian: arancini or arancine) are stuffed rice balls which are coated with bread crumbs and then deep fried. They are usually filled with ragù (meat and tomato sauce), mozzarella, and peas. A number of regional variants exist which differ in fillings and shape. The name, which is translated as 'little orange', derives from their shape and colour, which is reminiscent of an orange after cooking. Arancini produced in eastern Sicily (especially in Catania) have a more conical shape.",
-        date: moment().subtract(1, 'days').calendar(),
+        date: moment().subtract(2, 'days').calendar(),
         votes: 5,
         comments: [{
             author: "Bob",
@@ -22,7 +22,7 @@ app.controller("redditController", ($scope) => {
         author: "Ali Sue Hobbs",
         imgURL: "http://dh3k79jmajxjl.cloudfront.net/wp-content/uploads/2014/11/19223940/Plate.jpg",
         description: "Directions. Special equipment: 35 to 40 toothpicks. Preheat the oven to 425 degrees F. Stuff each date with 1 to 2 almonds. Wrap each date with half a piece of bacon and secure with a toothpick. Bake, turning the dates halfway through so the bacon is evenly cooked, 15 to 18 minutes. Serve warm or at room temperature.",
-        date: moment().subtract(2, 'days').calendar(),
+        date: moment().subtract(1, 'days').calendar(),
         votes: 1,
         comments: []
     }, {
@@ -31,7 +31,7 @@ app.controller("redditController", ($scope) => {
         author: "Ali Sue Hobbs",
         imgURL: "http://www.girlmakesfood.com/wp-content/uploads/2012/08/Vegan-Spring-Rolls.jpg",
         description: "Gỏi cuốn. Gỏi cuốn, Vietnamese spring roll, is a Vietnamese dish traditionally consisting of pork, prawn, vegetables, bún (rice vermicelli), and other ingredients wrapped in Vietnamese bánh tráng (commonly known as rice paper).",
-        date: moment().subtract(3, 'days').calendar(),
+        date: moment().subtract(1, 'days').calendar(),
         votes: -1,
         comments: []
     }]
