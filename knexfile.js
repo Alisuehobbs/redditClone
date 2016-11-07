7c59b6bfd1820014c5395b18d8edbec8f1843e41
Ali Hobbs <alisuehobbs@gmail.com>
2016-11-07 14:24:49 -0700

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,24 @@
 'use strict';
 
 module.exports = {
-        development: {
-            client: 'pg',
-            connection: 'postgres://localhost/reddit_dev',
-            migrations: {
-                directory: './server/db/migrations'
-            },
-            seeds: {
-                directory: './server/db/seeds'
-            }
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/reddit_dev',
+        migrations: {
+            directory: './server/db/migrations'
         },
-        production: {
-            client: 'pg',
-            connection: process.env.DATABASE_URL,
-            migrations: {
-                directory: './server/db/migrations'
-            },
-            seeds: {
-                directory: './server/db/seeds'
-            }
-        };
+        seeds: {
+            directory: './server/db/seeds'
+        }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: './server/db/migrations'
+        },
+        seeds: {
+            directory: './server/db/seeds'
+        }
+    }
+}
