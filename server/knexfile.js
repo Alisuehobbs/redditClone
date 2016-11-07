e802f1c8ff41b92fdd56abe5c556a3384ef86320
Ali Hobbs <alisuehobbs@gmail.com>
2016-11-07 13:50:30 -0700

    getting heroku set up

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,22 +3,10 @@
 module.exports = {
     development: {
         client: 'pg',
-        connection: 'postgres://localhost/reddit_dev',
-        // migrations: {
-        //     directory: './server/db/migrations'
-        // },
-        // seeds: {
-        //     directory: './server/db/seeds'
-        // }
+        connection: 'postgres://localhost/reddit_dev'
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
-    //     migrations: {
-    //         directory: './server/db/migrations'
-    //     },
-    //     seeds: {
-    //         directory: './server/db/seeds'
-    //     }
+        connection: process.env.DATABASE_URL
     }
 };
